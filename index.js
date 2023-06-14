50cf67c3cd971b8cef97ed963b47a8c3cb087eb8
lydia tschoe <lydiatschoe@gmail.com>
2023-06-14 15:50:18 -0400

    index.html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainToRemove = document.getElementById("main");
+if (mainToRemove){
+    mainToRemove.parentNode.removeChild(main)
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent= 'Lydia Tschoe is the champion!';
+newHeader.appendChild(newHeader)
